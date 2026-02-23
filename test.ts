a27b5131e1a6d51beacabac6e737cc09f52fc808
gsdgordon <33521592+gsdgordon@users.noreply.github.com>
2026-02-23 17:07:04 +0000

    Delete test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-StemBit_IR.init(DigitalPin.P0)
-
-StemBit_IR.onPressEvent(RemoteButton.Power, function () {
-    basic.showIcon(IconNames.Heart)
-})
-
-StemBit_IR.onPressEvent(RemoteButton.Up, function () {
-    basic.showArrow(ArrowNames.North)
-})
